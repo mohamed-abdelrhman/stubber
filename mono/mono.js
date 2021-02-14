4b64093fe00c94c0b09b90c1990b59d101a0698c
Mohamed <m.abdelrhman@baianat.com>
2021-02-14 20:45:22 +0200

    feat:specs files for graphql stubbers

--- mono/mono.js
+++ mono/mono.js
@@ -21,19 +21,18 @@ const call = async function (argv) {
     await createFolder('src/models')
     await createFolder('src/models/'+folderName)
     let crud=argv.crud;
-  await createAllMonoFiles(moduleName, folderName, fileName,crud)
+    let spec=false;
+    if (argv.spec) spec=true;
 
-}
-
-const createAllMonoFiles=  function (moduleName,folderName,fileName,crud){
     createModuleFile(moduleName,folderName,fileName,crud)
-    createServiceFile(moduleName,folderName,fileName,crud)
+    createServiceFile(moduleName,folderName,fileName,crud,spec)
     createRepositoryFile(moduleName,folderName,fileName,crud)
-    createResolverFile(moduleName,folderName,fileName,crud)
+    createResolverFile(moduleName,folderName,fileName,crud,spec)
     createEntityFile(moduleName,folderName,fileName,crud)
     createTypeFile(moduleName,folderName,fileName,crud)
     createInputFile(moduleName,folderName,fileName,crud)
 }
+
 
 
 const createModuleFile =  function(moduleName,folderName,fileName,crud=false){
@@ -42,11 +41,15 @@ const createModuleFile =  function(moduleName,folderName,fileName,crud=false){
     let stubPath = __dirname+'/stubs'+stubType+'/Dummy.'+fileTypes.module+'.stub'
      createFile(fileTypes.module,moduleName,folderName,fileName,stubPath)
 }
-const createServiceFile =  function(moduleName,folderName,fileName,crud=false){
+const createServiceFile =  function(moduleName,folderName,fileName,crud=false,spec=false){
     let stubType='/singles'
     if (crud)stubType='/crud';
     let stubPath = __dirname+'/stubs'+stubType+'/Dummy.'+fileTypes.service+'.stub'
      createFile(fileTypes.service,moduleName,folderName,fileName,stubPath)
+    if (spec){
+        let specStubPath = __dirname+'/stubs'+stubType+'/Dummy.'+fileTypes.service+'.spec.stub'
+        createFile(fileTypes.service+'.spec',moduleName,folderName,fileName,specStubPath)
+    }
 }
 const createRepositoryFile =  function(moduleName,folderName,fileName,crud=false){
     let stubType='/singles'
@@ -54,11 +57,16 @@ const createRepositoryFile =  function(moduleName,folderName,fileName,crud=false
     let stubPath = __dirname+'/stubs'+stubType+'/Dummy.'+fileTypes.repository+'.stub'
      createFile(fileTypes.repository,moduleName,folderName,fileName,stubPath)
 }
-const createResolverFile =  function(moduleName,folderName,fileName,crud=false){
+const createResolverFile =  function(moduleName,folderName,fileName,crud=false,spec=false){
     let stubType='/singles'
     if (crud)stubType='/crud';
     let stubPath = __dirname+'/stubs'+stubType+'/Dummy.'+fileTypes.resolver+'.stub'
      createFile(fileTypes.resolver,moduleName,folderName,fileName,stubPath)
+    if (spec){
+        let specStubPath = __dirname+'/stubs'+stubType+'/Dummy.'+fileTypes.resolver+'.spec.stub'
+        createFile(fileTypes.resolver+'.spec',moduleName,folderName,fileName,specStubPath)
+    }
+
 }
 
 const createEntityFile =  function(moduleName,folderName,fileName,crud=false){
